7b8da63d004ab76d82e30c4ef480305e7f8dae63
David Mihal <eastsideski@gmail.com>
2016-03-07 13:16:32 -0500

    Add icon

--- src/browser_action/server-sniffer.js
+++ src/browser_action/server-sniffer.js
@@ -5,6 +5,23 @@
     8000,
     8080
   ];
+  var itemBuilder = function(host, port, titleTxt) {
+    var li = document.createElement('li');
+    var link = document.createElement('a');
+    li.appendChild(link);
+    link.href = `http://${host}:${port}/`;
+    var img = document.createElement('img');
+    img.src = `http://${host}:${port}/favicon.ico`;
+    img.onerror = function(){
+      this.src = chrome.extension.getURL('icons/icon16.png');
+    };
+    link.appendChild(img);
+    var title = document.createElement('div');
+    title.innerText = titleTxt;
+    link.appendChild(title);
+    return li;
+  };
+
   var titleRegex = /<title>(.*?)<\/title>/;
   PORTS.forEach(function (port) {
     var xhr = new XMLHttpRequest();
@@ -13,9 +30,7 @@
       if (this.status >= 200 && this.status < 400) {
         var regexResult = titleRegex.exec(this.responseText);
         var title = regexResult ? regexResult[1] : this.responseURL;
-        var li = document.createElement('li');
-        li.innerHTML = '<a href="' + this.responseURL + '" target="_blank">' +
-            title + '</a>';
+        var li = itemBuilder('localhost', port, title);
         list.appendChild(li);
       }
     };
